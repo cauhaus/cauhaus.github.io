c0ef57d21d980054fc42ad238693ae3ae1be71a1
kiranmerchant <merchant@iplytics.com>
2019-06-26 03:12:37 +0200

    Updates

---
+++ precache-manifest.4265ecf805f9a50fd18633cc2a33bd4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5859315cc193ee58ac2e1e87e96109",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04dbc92c9a47bf003bb2",
+    "url": "/static/js/2.8054903b.chunk.js"
+  },
+  {
+    "revision": "b8ca365db7bb5336d811",
+    "url": "/static/js/main.e1b032b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2be4a2feb883724835a929dc945f5eb0",
+    "url": "/static/media/cow.2be4a2fe.png"
+  }
+]);
